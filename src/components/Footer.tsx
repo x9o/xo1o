05ab2ae615c7fc3230337fa95b5d509c67a36624
x9o <sunny456twist@gmail.com>
2025-10-16 19:15:14 +0800

    change of favicon and footer bg color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-8 px-4 border-t border-border">
+    <footer className="py-8 px-4 border-t border-border bg-black">
       <div className="container mx-auto text-center">
         <p className="text-muted-foreground">
           © 2025 <span className="gradient-text font-semibold">xo1o</span>. All rights reserved.
