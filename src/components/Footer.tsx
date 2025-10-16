ab15c5f091fad46ab86267ef87a3e01b08b6aba8
x9o <sunny456twist@gmail.com>
2025-10-16 19:27:23 +0800

    small changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-8 px-4 border-t border-border bg-black">
       <div className="container mx-auto text-center">
         <p className="text-muted-foreground">
-          © 2025 <span className="gradient-text font-semibold">xo1o</span>. All rights reserved.
+          © 2025 Website created by <span className="gradient-text font-semibold">xo1o</span>. All rights reserved.
         </p>
        
       </div>
